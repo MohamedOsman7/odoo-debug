80bfdb32fd5de650a84336682aafe2a536a64627
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2023-03-05 21:34:04 +0000

    Fix first startup issue

--- .devcontainer/web/startup.sh
+++ .devcontainer/web/startup.sh
@@ -8,6 +8,12 @@ if ! test -f /etc/odoo/odoo.conf; then
   #chmod g+w /etc/odoo/odoo.conf
 fi
 
+while ! test -d /src/odoo/odoo-bin; do
+  echo "Waiting for odoo checkout"
+  sleep 1000
+done
+
+echo "Source availble"
 /usr/bin/python3 /tmp/debugpy --listen 0.0.0.0:8888 /src/odoo/odoo-bin -c /etc/odoo/odoo.conf --db_user=$db_user --db_host=$db_host --db_password=$db_password
 
 exit 1
