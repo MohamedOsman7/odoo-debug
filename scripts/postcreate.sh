9b341872c2ea359efeebcbcf26caeda519043a58
Christopher Rogos <christopher.rogos@glueckkanja-gab.com>
2023-03-09 18:57:59 +0100

    Remove dependencies to private repositories

--- scripts/postcreate.sh
+++ scripts/postcreate.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
 echo "Running postcreate.sh script"
-cp /repo/.vscode/* /workspace/.vscode/
+sudo chown vscode:vscode /workspace
+
+cp -r /repo/.vscode /workspace/
 cp /repo/.env /workspace/.env
 
-sudo chown vscode:vscode workspace/
+
 
 if ! test -d /workspace/odoo; then
     echo "Cloning git repositories"
     cd /workspace
     git clone -b 15.0 --single-branch https://github.com/odoo/odoo.git odoo
-    git clone -b master https://github.com/c4a8-odoo/prod.git user
-    cd /workspace/user
-    git submodule update --init
+    # Add additional repositories here
+    # git clone -b master https://github.com/oca/timesheet.git timesheet
+    # ^^
+    # cd /workspace/timesheet
+    # git submodule update --init
 else
     echo "Git repositories already cloned"
 fi
